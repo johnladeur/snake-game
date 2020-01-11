81e76eb40c579c231b7962922a1b6cf58aab01e7
John LaDeur <jdladeur@gmail.com>
2020-01-11 15:47:29 -0600

    create snake figure, setInterval function, apple object

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 var canvas;
 var canvasContext;
+var snake = 50;
 
 window.onload = function(){
     this.console.log("Hello World!")
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+
+    var framesPerSecond = 30;
+    setInterval(function(){
+        moveEverything();
+        drawEverything();
+    }, 1000/framesPerSecond)
     
 }
 
-function drawEverything(){
+function moveEverything(){
+    snake = snake + 5;
+    if (snake > 800) {
     
+    }
+
+}
+
+function drawEverything(){
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(snake,210, 80, 15);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(250 ,210, 15, 15);
+
 }
